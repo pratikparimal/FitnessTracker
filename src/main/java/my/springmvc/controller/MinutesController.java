290b5ba129a686b377ea98d173d4feeb7cc93a5b
Pratik Parimal <Pratik Parimal@Pratik_Parimal>
2018-06-11 08:45:54 +0530

    updated code till 57

--- src/main/java/my/springmvc/controller/MinutesController.java
+++ src/main/java/my/springmvc/controller/MinutesController.java
@@ -1,15 +1,27 @@
 package my.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import my.springmvc.model.Exercise;
 
 @Controller
 public class MinutesController {
 
 	@RequestMapping(value="/addMinutes")
-	public String addMinutes(){
+	public String addMinutes(@ModelAttribute("exercise") Exercise exercise){
+		
+		System.out.println("Execrice: "+exercise.getMinutes());
 		return "addMinutes";
+		//return "forward:addMoreMinutes.html";
+		//return "redirect:addMoreMinutes.html";
 	}
 	
-	
+	/*@RequestMapping(value="/addMoreMinutes")
+	public String addMoreMinutes(@ModelAttribute("exercise") Exercise exercise){
+		
+		System.out.println("More Execrice: "+exercise.getMinutes());
+		return "addMinutes";
+	}*/
 }
--- src/main/webapp/WEB-INF/jsp/addMinutes.jsp
+++ src/main/webapp/WEB-INF/jsp/addMinutes.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -9,5 +10,18 @@
 </head>
 <body>
 	<h1>Add Minutes Exercised</h1>
+	<form:form modelAttribute="exercise">
+		<table>
+			<tr>
+				<td><spring:message code="goal.text" /> </td>
+				<td><form:input path="minutes"/></td>
+			</tr>
+			<tr>
+				<td colspan="2">
+					<input type="submit" value="Enter Exercised"/>
+				</td>
+			</tr>
+		</table>
+	</form:form>
 </body>
 </html>
